1f2fdb9981d295bfc9eb7ce7d984be7ce6054847
karomag <karomag88@mail.ru>
2023-06-21 00:12:46 +0300

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,59 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
+    public static StringBuilder log = new StringBuilder();
     public static void main(String[] args) {
-        File gamesDir = new File("D:\\Games");
-        File src = new File(gamesDir, "src");
-        File res = new File(gamesDir, "res");
-        File savegames = new File(gamesDir, "savegames");
-        File temp = new File(gamesDir, "temp");
 
-        StringBuilder log = new StringBuilder();
-        log.append("Создан каталог " + src.getName() + ": " + src.mkdir() + "\n");
-        log.append("Создан каталог " + res.getName() + ": " + res.mkdir() + "\n");
-        log.append("Создан каталог " + savegames.getName() + ": " + savegames.mkdir() + "\n");
-        log.append("Создан каталог " + temp.getName() + ": " + temp.mkdir() + "\n");
+        List<String> directories = List.of(
+                "D:\\Games\\src",
+                "D:\\Games\\src\\main",
+                "D:\\Games\\src\\test",
+                "D:\\Games\\res",
+                "D:\\Games\\res\\drawables",
+                "D:\\Games\\res\\vectors",
+                "D:\\Games\\res\\icons",
+                "D:\\Games\\savegames",
+                "D:\\Games\\temp");
 
-        File mainDir = new File(src, "main");
-        File testDir = new File(src, "test");
-        log.append("Создан каталог " + mainDir.getName() + ": " + mainDir.mkdir() + "\n");
-        log.append("Создан каталог " + testDir.getName() + ": " + testDir.mkdir() + "\n");
-
-        File mainFile = new File(mainDir, "Main.java");
-        File utilsFile = new File(mainDir, "Utils.java");
-
-        try {
-            log.append("Создан файл " + mainFile.getName() + ": " + mainFile.createNewFile() + "\n");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            log.append("Создан файл " + utilsFile.getName() + ": " + utilsFile.createNewFile() + "\n");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        for (String dir:directories) {
+            saveDirectory(dir);
         }
 
-        File drawables = new File(res, "drawables");
-        File vectors = new File(res, "vectors");
-        File icons = new File(res, "icons");
-        log.append("Создан каталог " + drawables.getName() + ": " + drawables.mkdir() + "\n");
-        log.append("Создан каталог " + vectors.getName() + ": " + vectors.mkdir() + "\n");
-        log.append("Создан каталог " + icons.getName() + ": " + icons.mkdir() + "\n");
+        createFile("D:\\Games\\src\\main", "Main.java");
+        createFile("D:\\Games\\src\\main", "Utils.java");
+        createFile("D:\\Games\\temp", "temp.txt");
 
-        File tempFile = new File(temp, "temp.txt");
-        try {
-            log.append("Создан файл " + tempFile.getName() + ": " + tempFile.createNewFile() + "\n");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
-        try (FileWriter writer = new FileWriter(tempFile)) {
+        try (FileWriter writer = new FileWriter("D:\\Games\\temp\\temp.txt")) {
             writer.write(log.toString());
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
+    }
+
+    public static void saveDirectory(String path) {
+        File dir = new File(path);
+        if (dir.mkdirs()) {
+            log.append("Директория " + path + " создана успешно" + "\n");
+        } else {
+            log.append("Директория " + path + " не создана" + "\n");
+        }
+    }
+
+    public static void createFile(String path, String fileName) {
+        File file = new File(path, fileName);
+        try {
+            if (file.createNewFile()) {
+                log.append("Файл " + fileName + " создан успешно" + "\n");
+            } else {
+                log.append("Файл " + fileName + " не создан" + "\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
